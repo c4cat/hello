b9cc4cb7b5ce06608c1019318ea6efdb9fce0cb5
c4cat <313187850@qq.com>
2014-04-10 18:04:01 +0800

    Signed-off-by: c4cat <313187850@qq.com>

--- js/less.js
+++ js/less.js
@@ -5,24 +5,81 @@
 $(document).ready(function(){
 	//--- global ---///
 	//var s_tmp = $('#tmp').value();
+	function hide(){
+        $.ui.hideMask();
+    }
+
 	//--- submit setting ---//
 	$('#setting-submit').click(function(){
 		var tmp = $('#tmp').val(),
 			light = $('#light').val(),
-			open = $('input[name="swi"]:checked').val();
-			$.getJSON('http://cornelia.com.cn/i/post2.php?callback=?',{tmp:tmp,light:light,open:open},function(data){
-           	 		console.log(data);
+			open = $('input[name="swi"]:checked').val(),
+			type='update',
+			//red is the count of the error ,not more than one
+			red = $('#setting-form').find('.red').length,
+			blue = $('#setting-form').find('.blue').length;
+
+			//form check
+			if(tmp=='' || light=='' || open==''){
+				$.ui.hideMask();
+				if(red==0){
+					// $('.blue').remove();
+					$('#setting-form').append('<div class="input-group notice red">每一项都必须填写!</div>');
+				}else{
+					if(blue==1){
+						$('#setting-form').find('.blue').removeClass('blue').html('每一项都必须填写!!');
+					}
+				}
+				return false;
+			}
+
+			$.getJSON('http://cornelia.com.cn/i/action.php?callback=?',{tmp:tmp,light:light,open:open,type:type},function(data){
+           	 		console.log(123);
+           	 		if(red==0){
+           	 			$('#setting-form').append('<div class="input-group notice blue">成功!</div>');
+           	 		}else{
+           	 			$('#setting-form').find('.red').addClass('blue').html('成功!!');
+           	 		}
+           	 			$.ui.hideMask();
     				}
     			);
+			
 	});
+	
+
 	//--- add mode ---//
 	$('#addmode-submit').click(function(){
+		//form check
 		var tmp = $('#addmode-tmp').val(),
 			light = $('#addmode-light').val(),
 			open = $('input[name="addmode-swi"]:checked').val(),
-			name = $('#addmode-name').val();
-			$.getJSON('http://cornelia.com.cn/i/post2.php?callback=?',{name:name,tmp:tmp,light:light,open:open},function(data){
-           	 		console.log(data);
+			name = $('#addmode-name').val(),
+			type='addmode',
+			//red is the count of the error ,not more than one
+			red = $('#addmode-form').find('.red').length,
+			blue = $('#addmode-form').find('.blue').length;
+
+			//form check
+			if(tmp=='' || light=='' || open=='' || name==''){
+				$.ui.hideMask();
+				if(red==0){
+					// $('.blue').remove();
+					$('#addmode-form').append('<div class="input-group notice red">每一项都必须填写!</div>');
+				}else{
+					if(blue==1){
+						$('#addmode-form').find('.blue').removeClass('blue').html('每一项都必须填写!!');
+					}
+				}
+				return false;
+			}
+			$.getJSON('http://cornelia.com.cn/i/action.php?callback=?',{tmp:tmp,light:light,open:open,type:type,name:name},function(data){
+           	 		// $('#afui_mask').find('h1').html('成功!');
+           	 		if(red==0){
+           	 			$('#setting-form').append('<div class="input-group notice blue">成功!</div>');
+           	 		}else{
+           	 			$('#setting-form').find('.red').addClass('blue').html('成功!!');
+           	 		}
+           	 		$.ui.hideMask();
     				}
     			);
 	});
@@ -35,5 +92,11 @@ $(document).ready(function(){
 			$('#now-swi').html();
 		//}
 	});
+
+	//---remove---
+	$('.settings').click(function() {
+		$('.notice').remove();
+	});
+
 });
 
